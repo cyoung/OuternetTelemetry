4f45025ad471a79b67e3673f656a9c396899c4b9
Christopher Young <cyoung@gmail.com>
2016-10-12 09:40:37 -0400

    Calculate data rate once per 50 seconds (totaled for 1 minute). Average SNR over 50 sec, 1 sec samples.

--- statsServer.go
+++ statsServer.go
@@ -5,16 +5,63 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/paulbellamy/ratecounter"
+	"net"
 	"net/http"
 	"strconv"
 	"time"
 )
 
 const (
-	DATAPOINTS = 60
+	DATAPOINTS        = 1440
+	DATAUPLOAD_SERVER = "updates.stratux.me:9000"
 )
 
-var pps_counter = ratecounter.NewRateCounter(1 * time.Second) // Packets per second.
+var datarate_counter = ratecounter.NewRateCounter(1 * time.Minute) // Packets per minute.
+
+type StatsMessage struct {
+	TimeCollected time.Time // Ending timestamp for the collection period.
+	PeriodSeconds int       // Number of seconds for the collection period.
+	SNR_Avg       float64   // SNR average over the period.
+	Packets_Total int       // Packets transferred (total: success + error) over the period.
+}
+
+/*
+	statsPoster().
+	 Posts stats to remote server.
+*/
+var statsChannel chan StatsMessage
+
+func statsPoster(receiverLat, receiverLng float64) {
+	statsChannel = make(chan StatsMessage, 1024)
+	var conn *net.Conn
+	msg := ""
+	for {
+		conn, err := net.Dial("tcp", DATAUPLOAD_SERVER)
+		if err != nil {
+			fmt.Printf("statsPoster(): error connecting to '%s'\n", DATAUPLOAD_SERVER)
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		fmt.Printf("statsPoster(): connected to '%s'\n", DATAUPLOAD_SERVER)
+		for {
+			// This is here to account for a the last message received from the channel but not yet sent because of a network error.
+			if len(msg) == 0 {
+				// Get the new stat item.
+				d := <-statsChannel // Blocks.
+				json, _ := json.Marshal(&d)
+				msg = json + "\n"
+			}
+
+			_, err := conn.Write([]byte(msg))
+			if err != nil {
+				fmt.Printf("statsPoster(): write error: %s\n", err.Error())
+				time.Sleep(1 * time.Second)
+				break // Reconnect, try sending message again.
+			}
+			msg = "" // Success, clear buffer.
+		}
+	}
+}
 
 //http://www.jsgraphs.com/
 
@@ -28,8 +75,8 @@ func constructChartData(data []float64) ChartData {
 	ret.Series = make([][]float64, 1)
 	for i := 0; i < DATAPOINTS; i++ {
 		lbl := ""
-		if i%10 == 0 {
-			lbl = strconv.Itoa(i) + " sec"
+		if i%60 == 0 {
+			lbl = strconv.Itoa(i/60) + " hour"
 		}
 		ret.Labels = append(ret.Labels, lbl)
 		sData := float64(0)
@@ -50,10 +97,10 @@ func handleSNRRequest(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
-func handlePPSRequest(w http.ResponseWriter, r *http.Request) {
+func handleDataRateRequest(w http.ResponseWriter, r *http.Request) {
 	var data []float64
-	for i := 0; i < len(pps); i++ {
-		data = append(data, float64(pps[i]))
+	for i := 0; i < len(datarate); i++ {
+		data = append(data, float64(datarate[i]))
 	}
 	ret := constructChartData(data)
 
@@ -67,10 +114,19 @@ func defaultServer(w http.ResponseWriter, r *http.Request) {
 }
 
 var snr []float64
-var pps []int64
+var datarate []int64
+
+func avg(vals []float64) float64 {
+	var total float64
+	for _, v := range vals {
+		total += v
+	}
+	return total / float64(len(vals))
+}
 
 func TelemetryWatcher() {
-	ticker := time.NewTicker(1 * time.Second)
+	secondTicker := time.NewTicker(1 * time.Second)
+	minuteTicker := time.NewTicker(50 * time.Second)
 	ot, err := OuternetTelemetry.NewClient()
 	if err != nil {
 		fmt.Printf("err: %s\n", err.Error())
@@ -79,34 +135,45 @@ func TelemetryWatcher() {
 
 	var packetCountLast int
 
+	var curSNR []float64
+
 	for {
-		<-ticker.C
 		v, err := ot.GetStatus()
 		if err != nil {
 			fmt.Printf("err: %s\n", err.Error())
 			continue
 		}
-		snr = append(snr, v.Tuner.SNR)
-		if len(snr) > DATAPOINTS {
-			snr = snr[len(snr)-DATAPOINTS:]
-		}
+		select {
+		case <-secondTicker.C:
+			curSNR = append(curSNR, v.Tuner.SNR)
+		case <-minuteTicker.C:
+			// Average one-second SNR over the minute.
+			avgSNROneMinute := avg(curSNR)
+			curSNR = make([]float64, 0)
+			snr = append(snr, avgSNROneMinute)
+			if len(snr) > DATAPOINTS {
+				snr = snr[len(snr)-DATAPOINTS:]
+			}
 
-		// Count all packets, including "CRC_Err" packets.
-		curPackets := v.Tuner.CRC_OK + v.Tuner.CRC_Err
-		packetsSinceLast := curPackets - packetCountLast
-		if packetCountLast == 0 && len(pps) == 0 {
-			// This is the first count. We'll skip this measurement.
+			// Calculate packet rate over the last minute.
+
+			// Count all packets, including "CRC_Err" packets.
+			curPackets := v.Tuner.CRC_OK + v.Tuner.CRC_Err
+			packetsSinceLast := curPackets - packetCountLast
+			if packetCountLast == 0 && len(datarate) == 0 {
+				// This is the first count. We'll skip this measurement.
+				packetCountLast = curPackets
+				continue
+			}
 			packetCountLast = curPackets
-			continue
-		}
-		packetCountLast = curPackets
-		// Increment the counter.
-		pps_counter.Incr(int64(packetsSinceLast))
+			// Increment the counter.
+			datarate_counter.Incr(int64(packetsSinceLast))
 
-		// Get counter rate and save in the data slice.
-		pps = append(pps, pps_counter.Rate())
-		if len(pps) > DATAPOINTS {
-			pps = pps[len(pps)-DATAPOINTS:]
+			// Get counter rate and save in the data slice.
+			datarate = append(datarate, datarate_counter.Rate())
+			if len(datarate) > DATAPOINTS {
+				datarate = datarate[len(datarate)-DATAPOINTS:]
+			}
 		}
 	}
 }
@@ -114,7 +181,7 @@ func TelemetryWatcher() {
 func main() {
 	go TelemetryWatcher()
 	http.HandleFunc("/snr", handleSNRRequest)
-	http.HandleFunc("/pps", handlePPSRequest)
+	http.HandleFunc("/datarate", handleDataRateRequest)
 	http.HandleFunc("/", defaultServer)
 
 	err := http.ListenAndServe(":8080", nil)
